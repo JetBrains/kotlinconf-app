fa177f8d371f18527da3e2e2562847aafb430c6a
Roman Elizarov <elizarov@gmail.com>
2018-09-14 17:41:10 +0300

    Nicer impl for KotlinConfApi.createUser

--- common/src/main/kotlin/org/jetbrains/kotlinconf/api/KotlinConfApi.kt
+++ common/src/main/kotlin/org/jetbrains/kotlinconf/api/KotlinConfApi.kt
@@ -1,14 +1,12 @@
 package org.jetbrains.kotlinconf.api
 
 import io.ktor.client.*
-import io.ktor.client.call.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
+import io.ktor.client.response.*
 import io.ktor.http.*
-import kotlinx.coroutines.*
-import kotlinx.serialization.*
-import org.jetbrains.kotlinconf.*
+import kotlinx.io.core.*
 import org.jetbrains.kotlinconf.data.*
 
 internal expect val END_POINT: String
@@ -25,16 +23,12 @@ class KotlinConfApi(private val userId: String) {
         install(ExpectSuccess)
     }
 
-    suspend fun createUser(): Boolean {
-        val response = client.call {
-            url(urlString = END_POINT)
-            method = HttpMethod.Post
-            url.encodedPath = "users"
-            body = userId
-        }.response
-
-        response.close()
-        return response.status.isSuccess()
+    suspend fun createUser(): Boolean = client.request<HttpResponse> {
+        url("users", auth = false)
+        method = HttpMethod.Post
+        body = userId
+    }.use {
+        it.status.isSuccess()
     }
 
     suspend fun getAll(): AllData = client.get {
@@ -69,9 +63,10 @@ class KotlinConfApi(private val userId: String) {
         contentType(ContentType.Application.Json)
     }
 
-    private fun HttpRequestBuilder.url(path: String) {
-        header(HttpHeaders.Authorization, "Bearer $userId")
-        header(HttpHeaders.CacheControl, "no-cache")
+    private fun HttpRequestBuilder.url(path: String, auth: Boolean = true) {
+        if (auth) {
+            header(HttpHeaders.Authorization, "Bearer $userId")
+        }
         url {
             takeFrom(END_POINT)
             encodedPath = path
