554d6b937536687b076c122667b09a4e27433549
Jarek Ankowski <jarek.ankowski@gmail.com>
2018-09-16 23:34:55 +0100

    Change action bar icon when search expanded

--- android/app/src/main/java/org/jetbrains/kotlinconf/ui/MainActivity.kt
+++ android/app/src/main/java/org/jetbrains/kotlinconf/ui/MainActivity.kt
@@ -50,6 +50,15 @@ class MainActivity : AppCompatActivity(), AnkoComponent<Context>, NavigationMana
             searchView.isIconified = false
         }
 
+        searchView.setOnSearchClickListener {
+            supportActionBar?.setLogo(R.drawable.kotlinconf_logo)
+        }
+
+        searchView.setOnCloseListener {
+            supportActionBar?.setLogo(R.drawable.kotlinconf_logo_text)
+            return@setOnCloseListener false
+        }
+
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean = false
             override fun onQueryTextChange(newText: String): Boolean {
