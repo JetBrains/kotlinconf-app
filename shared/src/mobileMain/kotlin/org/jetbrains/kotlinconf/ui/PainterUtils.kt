5d04c81578e8b7b454846c492e3fb4722b580723
leonid.stashevsky <leonid.stashevsky@gmail.com>
2024-01-30 13:01:34 +0100

    Add svg map with gestures

--- shared/src/mobileMain/kotlin/org/jetbrains/kotlinconf/ui/PainterUtils.kt
+++ shared/src/mobileMain/kotlin/org/jetbrains/kotlinconf/ui/PainterUtils.kt
@@ -1,10 +1,11 @@
 package org.jetbrains.kotlinconf.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
 @OptIn(ExperimentalResourceApi::class)
-fun DrawableResource.painter() = painterResource(this)
+fun DrawableResource.painter(): Painter = painterResource(this)
