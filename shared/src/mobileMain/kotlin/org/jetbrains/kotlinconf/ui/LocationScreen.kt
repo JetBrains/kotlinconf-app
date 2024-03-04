01c1b212bdc97e574108961eb9a83cb6e4dc722c
leonid.stashevsky <leonid.stashevsky@gmail.com>
2024-03-04 09:05:56 +0100

    Add default locations

--- shared/src/mobileMain/kotlin/org/jetbrains/kotlinconf/ui/LocationScreen.kt
+++ shared/src/mobileMain/kotlin/org/jetbrains/kotlinconf/ui/LocationScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -16,19 +15,52 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.scale
 import androidx.compose.ui.graphics.drawscope.translate
-import androidx.compose.ui.graphics.nativeCanvas
 import kotlinconfapp.shared.generated.resources.Res
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.kotlinconf.ui.components.Tab
 import org.jetbrains.kotlinconf.ui.components.TabBar
 
+enum class Floor(
+    override val title: String,
+    val resource: String,
+    val initialOffset: Offset,
+    val minOffset: Offset,
+    val maxOffset: Offset,
+    val initialScale: Float,
+    val minScale: Float,
+    val maxScale: Float
+) : Tab {
+    FIRST(
+        "1st floor",
+        "files/map-first.svg",
+        Offset(0f, 0f),
+        Offset(-1000f, -1000f),
+        Offset(1000f, 1000f),
+        1.0f,
+        0.5f,
+        2f
+    ),
+    SECOND(
+        "2nd floor", "files/map-second.svg",
+        Offset(0f, 0f),
+        Offset(-1000f, -1000f),
+        Offset(1000f, 1000f),
+        1.0f,
+        0.5f,
+        2f
+    );
 
-enum class Floor(override val title: String, val resource: String) : Tab {
-    FIRST("1st floor", "files/map-first.svg"),
-    SECOND("2nd floor", "files/map-second.svg")
+    fun coerceScale(scale: Float): Float {
+        return scale.coerceIn(minScale, maxScale)
+    }
+
+    fun coerceTranslate(translate: Offset): Offset {
+        val x = translate.x.coerceIn(minOffset.x, maxOffset.x)
+        val y = translate.y.coerceIn(minOffset.y, maxOffset.y)
+        return Offset(x, y)
+    }
 }
 
 @OptIn(ExperimentalResourceApi::class)
@@ -41,12 +73,15 @@ fun LocationScreen() {
         svgString = Svg(Res.readBytes(floor.resource))
     }
 
-    var scale by remember { mutableFloatStateOf(1f) }
-    var offset by remember { mutableStateOf(Offset(0f, 0f)) }
+    var scale by remember { mutableFloatStateOf(floor.initialScale) }
+    var translate by remember { mutableStateOf(Offset(0f, 0f)) }
     val state = rememberTransformableState { zoomChange, offsetChange, _ ->
         scale *= zoomChange
-        offset += offsetChange
+        translate += offsetChange
     }
+
+    val offset = floor.coerceTranslate(floor.initialOffset + translate)
+    scale = floor.coerceScale(scale)
 
     Box {
         Canvas(
