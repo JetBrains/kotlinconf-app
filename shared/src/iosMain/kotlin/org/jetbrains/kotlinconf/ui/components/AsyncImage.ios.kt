a5a47f23f0a1b59127f211cc8ab9ecfd2b12f718
leonid.stashevsky <leonid.stashevsky@gmail.com>
2024-03-05 09:48:19 +0100

    Use compose image loader to load images

--- shared/src/iosMain/kotlin/org/jetbrains/kotlinconf/ui/components/AsyncImage.ios.kt
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.kotlinconf.ui.components
-
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.toComposeImageBitmap
-import org.jetbrains.skia.Image
-
-internal actual fun ByteArray.asBitmap(): ImageBitmap = Image.makeFromEncoded(this)
-    .toComposeImageBitmap()
