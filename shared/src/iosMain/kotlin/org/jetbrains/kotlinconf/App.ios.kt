e7284c47109ea6258e86a3f45c2cbc6284e98bc0
leonid.stashevsky <leonid.stashevsky@gmail.com>
2024-03-05 10:29:35 +0100

    Add image cache

---
+++ shared/src/iosMain/kotlin/org/jetbrains/kotlinconf/App.ios.kt
@@ -0,0 +1,42 @@
+package org.jetbrains.kotlinconf
+
+import com.seiko.imageloader.ImageLoader
+import com.seiko.imageloader.component.setupDefaultComponents
+import com.seiko.imageloader.intercept.bitmapMemoryCacheConfig
+import com.seiko.imageloader.intercept.imageMemoryCacheConfig
+import com.seiko.imageloader.intercept.painterMemoryCacheConfig
+import okio.Path.Companion.toPath
+import platform.Foundation.NSCachesDirectory
+import platform.Foundation.NSSearchPathForDirectoriesInDomains
+import platform.Foundation.NSUserDomainMask
+
+actual fun createImageLoader(context: ApplicationContext): ImageLoader {
+    return ImageLoader {
+        components {
+            setupDefaultComponents()
+        }
+        interceptor {
+            bitmapMemoryCacheConfig {
+                maxSize(64 * 1024 * 1024) // 32MB
+            }
+            imageMemoryCacheConfig {
+                maxSize(50)
+            }
+            painterMemoryCacheConfig {
+                maxSize(50)
+            }
+            diskCacheConfig {
+                directory(getCacheDir().toPath().resolve("image_cache"))
+                maxSizeBytes(512L * 1024 * 1024) // 512MB
+            }
+        }
+    }
+}
+
+private fun getCacheDir(): String {
+    return NSSearchPathForDirectoriesInDomains(
+        NSCachesDirectory,
+        NSUserDomainMask,
+        true,
+    ).first() as String
+}
