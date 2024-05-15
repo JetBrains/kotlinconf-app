9c5a45aa5dd7f4b4cd82f302810f60ff794c9eea
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2024-05-15 13:38:36 +0200

    fixup! Remove unused Dispatchers.App

--- shared/src/iosMain/kotlin/org/jetbrains/kotlinconf/utils/Dispatchers.ios.kt
+++ shared/src/iosMain/kotlin/org/jetbrains/kotlinconf/utils/Dispatchers.ios.kt
@@ -2,32 +2,6 @@ package org.jetbrains.kotlinconf.utils
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.IO as kxIO
-import platform.darwin.*
-import kotlin.coroutines.CoroutineContext
 
 actual val Dispatchers.IO_MP: CoroutineDispatcher
     get() = kxIO
-
-@OptIn(InternalCoroutinesApi::class)
-private object AppDispatcher : CoroutineDispatcher(), Delay {
-    override fun dispatch(context: CoroutineContext, block: Runnable) {
-        val queue = dispatch_get_main_queue()
-        dispatch_async(queue) {
-            block.run()
-        }
-    }
-
-    override fun scheduleResumeAfterDelay(
-        timeMillis: Long,
-        continuation: CancellableContinuation<Unit>
-    ) {
-        val queue = dispatch_get_main_queue()
-
-        val time = dispatch_time(DISPATCH_TIME_NOW, (timeMillis * NSEC_PER_MSEC.toLong()))
-        dispatch_after(time, queue) {
-            with(continuation) {
-                resumeUndispatched(Unit)
-            }
-        }
-    }
-}
