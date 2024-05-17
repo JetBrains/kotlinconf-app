96f49ff2984bfd33b654534fe78fc90b27f80b6e
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-05-17 19:35:42 +0200

    Add notification manager for web

--- shared/src/webMain/kotlin/org/jetbrains/kotlinconf/NotificationManager.web.kt
+++ shared/src/webMain/kotlin/org/jetbrains/kotlinconf/NotificationManager.web.kt
@@ -1,17 +1,62 @@
 package org.jetbrains.kotlinconf
 
-// TODO implement
+private external object Notification {
+    fun requestPermission(callback: (String) -> Unit)
+}
+
+private fun registerNotificationByServiceWorker(delay: Long, title: String, message: String): Unit =
+    js(
+        """{
+        if (typeof navigator === "undefined" || !navigator.serviceWorker?.ready) return;
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.active.postMessage({
+            command: 'register-notification',
+            title: title,
+            body: message,
+            delay: Number(delay),
+          });
+        });
+        }"""
+    )
+
+private fun cancelNotificationByServiceWorker(title: String): Unit =
+    js(
+        """{
+        if (typeof navigator === "undefined" || !navigator.serviceWorker?.ready) return;
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.active.postMessage({
+            command: 'cancel-notification',
+            title: title,
+          });
+        });
+        }"""
+    )
+
 actual class NotificationManager actual constructor(
     context: ApplicationContext
 ) {
+    private var notificationAllowed = false
+
     actual fun requestPermission() {
+        Notification.requestPermission {
+            if (it == "granted") {
+                notificationAllowed = true
+            } else {
+                notificationAllowed = false
+            }
+        }
     }
 
     actual fun schedule(delay: Long, title: String, message: String): String? {
-        return null
+        if (!notificationAllowed) return null
+
+        registerNotificationByServiceWorker(delay, title, message)
+        return title
     }
 
     actual fun cancel(title: String) {
+        if (!notificationAllowed) return
+        cancelNotificationByServiceWorker(title)
     }
 
 }
