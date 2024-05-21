3115aa545e969961a550c6cf45806012bb8089b8
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2024-05-22 00:41:39 +0200

    Web: add an ability to customize proxy server for images from sessionize.com

--- shared/src/webMain/kotlin/org/jetbrains/kotlinconf/App.web.kt
+++ shared/src/webMain/kotlin/org/jetbrains/kotlinconf/App.web.kt
@@ -6,11 +6,16 @@ import com.seiko.imageloader.component.setupDefaultComponents
 import com.seiko.imageloader.intercept.bitmapMemoryCacheConfig
 import com.seiko.imageloader.intercept.imageMemoryCacheConfig
 import com.seiko.imageloader.intercept.painterMemoryCacheConfig
+import kotlinx.browser.window
+import org.w3c.dom.url.URL
+
+val defaultSessionizeProxy = apiEndpoint 
 
 actual fun createImageLoader(context: ApplicationContext): ImageLoader {
     return ImageLoader {
         components {
-            add(SessionizeUrlsMapper()) //TODO
+            val proxy = URL(window.location.href).searchParams.get("sessionizeProxy") ?: defaultSessionizeProxy
+            add(SessionizeUrlsMapper(proxy))
             setupDefaultComponents { HTTP_CLIENT }
         }
         interceptor {
--- shared/src/webMain/kotlin/org/jetbrains/kotlinconf/SessionizeUrlsMapper.kt
+++ shared/src/webMain/kotlin/org/jetbrains/kotlinconf/SessionizeUrlsMapper.kt
@@ -5,12 +5,12 @@ import com.seiko.imageloader.option.Options
 import io.ktor.http.*
 
 
-class SessionizeUrlsMapper : Mapper<Url> {
+class SessionizeUrlsMapper(private val sessionizeProxy: String) : Mapper<Url> {
     override fun map(data: Any, options: Options): Url? {
         if (data !is String) return null
         if (!isApplicable(data)) return null
         if (data.startsWith("https://sessionize.com/")) {
-            val newUri = data.replace("https://sessionize.com/",  "$apiEndpoint/sessionize/")
+            val newUri = data.replace("https://sessionize.com/",  "$sessionizeProxy/sessionize/")
             return Url(newUri)
         }
         return Url(data)
