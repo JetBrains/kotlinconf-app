dfb80778904de4d8fb10b1d23442bae3549bcc17
Márton Braun <marton.braun@jetbrains.com>
2024-10-21 10:00:02 +0200

    Add colors and styles, initial drafts of components

---
+++ shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui25/components/KCText.kt
@@ -0,0 +1,23 @@
+package org.jetbrains.kotlinconf.ui25.components
+
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import org.jetbrains.kotlinconf.ui25.theme.KotlinConfTheme
+
+@Composable
+fun KCText(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = KotlinConfTheme.colors.primaryText,
+    style: TextStyle = KotlinConfTheme.typography.text1,
+) {
+    BasicText(
+        text = text,
+        modifier = modifier,
+        style = style,
+        color = { color }
+    )
+}
---
+++ shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui25/components/SectionTitle.kt
@@ -0,0 +1,30 @@
+package org.jetbrains.kotlinconf.ui25.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.jetbrains.kotlinconf.ui25.theme.KotlinConfTheme
+import org.jetbrains.kotlinconf.ui25.theme.PreviewHelper
+
+@Composable
+fun SectionTitle(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    KCText(
+        text = text,
+        modifier = modifier,
+        style = KotlinConfTheme.typography.h2,
+        color = KotlinConfTheme.colors.primaryText,
+    )
+}
+
+@Preview
+@Composable
+fun SectionTitlePreview() {
+    PreviewHelper {
+        SectionTitle("Section title")
+        SectionTitle("7:30")
+    }
+}
