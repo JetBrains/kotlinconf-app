a86896c56b53d9c680bf1546ccf32e516958bfbf
Márton Braun <marton.braun@jetbrains.com>
2024-11-15 22:55:49 +0100

    Add second set of components

--- ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/components/Switcher.kt
+++ ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/components/Switcher.kt
@@ -4,9 +4,19 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -25,9 +35,18 @@ private fun SwitcherItem(
     selected: Boolean,
     modifier: Modifier = Modifier,
 ) {
-    val backgroundColor by animateColorAsState(if (selected) Color.Transparent else KotlinConfTheme.colors.tileBackground)
-    val textColor by animateColorAsState(if (selected) KotlinConfTheme.colors.primaryText else KotlinConfTheme.colors.secondaryText)
-    val strokeColor by animateColorAsState(if (selected) KotlinConfTheme.colors.strokeFull else Color.Transparent)
+    val backgroundColor by animateColorAsState(
+        if (selected) Color.Transparent
+        else KotlinConfTheme.colors.tileBackground,
+    )
+    val textColor by animateColorAsState(
+        if (selected) KotlinConfTheme.colors.primaryText
+        else KotlinConfTheme.colors.secondaryText,
+    )
+    val strokeColor by animateColorAsState(
+        if (selected) KotlinConfTheme.colors.strokeFull
+        else Color.Transparent,
+    )
 
     Box(
         modifier = modifier
--- ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/theme/PreviewUtil.kt
+++ ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/theme/PreviewUtil.kt
@@ -1,7 +1,10 @@
 package org.jetbrains.kotlinconf.ui.theme
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
