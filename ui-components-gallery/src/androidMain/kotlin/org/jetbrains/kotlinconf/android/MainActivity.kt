f9e008a815e0648584e323be61e5f0d3a246d879
Márton Braun <marton.braun@jetbrains.com>
2024-11-26 00:41:18 +0100

    Improve gallery setup

--- ui-components-gallery/src/androidMain/kotlin/org/jetbrains/kotlinconf/android/MainActivity.kt
+++ ui-components-gallery/src/androidMain/kotlin/org/jetbrains/kotlinconf/android/MainActivity.kt
@@ -3,13 +3,23 @@ package org.jetbrains.kotlinconf.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.ui.Modifier
 import org.jetbrains.kotlinconf.ui.components.GalleryApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GalleryApp()
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .safeDrawingPadding()
+            ) {
+                GalleryApp()
+            }
         }
     }
 }
