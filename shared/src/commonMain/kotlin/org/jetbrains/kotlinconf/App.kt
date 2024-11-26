4aec7863fbd18559bcb1cac3680e5a1238e76764
Márton Braun <marton.braun@jetbrains.com>
2024-11-26 01:04:43 +0100

    Clean up old imageloader

--- shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/App.kt
+++ shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/App.kt
@@ -4,12 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.seiko.imageloader.ImageLoader
-import com.seiko.imageloader.LocalImageLoader
 import org.jetbrains.kotlinconf.ui.components.StyledText
 import org.jetbrains.kotlinconf.ui.theme.KotlinConfTheme
 
@@ -21,19 +18,12 @@ fun App(context: ApplicationContext) {
         val service = remember {
             ConferenceService(context, apiEndpoint)
         }
-
-        CompositionLocalProvider(
-            LocalImageLoader provides remember { createImageLoader(context) },
+        Box(
+            Modifier.fillMaxSize()
+                .background(KotlinConfTheme.colors.mainBackground),
+            contentAlignment = Alignment.Center
         ) {
-            Box(
-                Modifier.fillMaxSize()
-                    .background(KotlinConfTheme.colors.mainBackground),
-                contentAlignment = Alignment.Center
-            ) {
-                StyledText("Placeholder")
-            }
+            StyledText("Placeholder")
         }
     }
 }
-
-expect fun createImageLoader(context: ApplicationContext): ImageLoader
