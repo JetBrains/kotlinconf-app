d6a9b1197f0e17ff8a13049f4e65eaa6d0a1a2d5
Márton Braun <marton.braun@jetbrains.com>
2025-01-14 23:59:12 +0100

    Set up hot reload in shared module

--- shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/App.kt
+++ shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/App.kt
@@ -36,78 +36,87 @@ import org.jetbrains.kotlinconf.screens.Speaker
 import org.jetbrains.kotlinconf.ui.components.StyledText
 import org.jetbrains.kotlinconf.ui.theme.KotlinConfTheme
 import kotlin.reflect.typeOf
+import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 const val apiEndpoint = "https://kotlinconf-app-prod.labs.jb.gg"
 
 @Composable
 fun App(context: ApplicationContext) {
     val navController = rememberNavController()
-    KotlinConfTheme {
-        val service = remember {
-            ConferenceService(context, apiEndpoint)
-        }
-        CompositionLocalProvider(LocalNavController provides navController) {
-            Box(
-                Modifier.fillMaxSize()
-                    .background(KotlinConfTheme.colors.mainBackground),
-                contentAlignment = Alignment.Center
-            ) {
-                NavHost(
-                    navController = navController,
-                    startDestination = StartScreen,
-                    modifier = Modifier.fillMaxSize(),
-                    enterTransition = enterTransition { it },
-                    exitTransition = exitTransition { -it },
-                    popEnterTransition = enterTransition { -it },
-                    popExitTransition = exitTransition { it },
+    DevelopmentEntryPoint {
+        KotlinConfTheme {
+            val service = remember {
+                ConferenceService(context, apiEndpoint)
+            }
+            CompositionLocalProvider(LocalNavController provides navController) {
+                Box(
+                    Modifier.fillMaxSize()
+                        .background(KotlinConfTheme.colors.mainBackground),
+                    contentAlignment = Alignment.Center
                 ) {
-                    composable<StartScreen> {
-                        Column {
-                            StyledText("Start screen")
-                            StyledText("About App", Modifier.clickable { navController.navigate(AboutAppScreen) })
-                            StyledText("About Conference", Modifier.clickable { navController.navigate(AboutConferenceScreen) })
-                            StyledText("Code of Conduct", Modifier.clickable { navController.navigate(CodeOfConductScreen) })
-                            StyledText("Settings", Modifier.clickable { navController.navigate(SettingsScreen) })
-                            StyledText("Terms of use", Modifier.clickable { navController.navigate(TermsOfUseScreen) })
-                            StyledText("Partners", Modifier.clickable { navController.navigate(PartnersScreen) })
-                            StyledText("Schedule", Modifier.clickable { navController.navigate(ScheduleScreen) })
-                            StyledText("Speakers", Modifier.clickable { navController.navigate(SpeakersScreen) })
+                    NavHost(
+                        navController = navController,
+                        startDestination = StartScreen,
+                        modifier = Modifier.fillMaxSize(),
+                        enterTransition = enterTransition { it },
+                        exitTransition = exitTransition { -it },
+                        popEnterTransition = enterTransition { -it },
+                        popExitTransition = exitTransition { it },
+                    ) {
+                        composable<StartScreen> {
+                            Column {
+                                StyledText("Start screen")
+                                StyledText("About App", Modifier.clickable { navController.navigate(AboutAppScreen) })
+                                StyledText(
+                                    "About Conference",
+                                    Modifier.clickable { navController.navigate(AboutConferenceScreen) })
+                                StyledText(
+                                    "Code of Conduct",
+                                    Modifier.clickable { navController.navigate(CodeOfConductScreen) })
+                                StyledText("Settings", Modifier.clickable { navController.navigate(SettingsScreen) })
+                                StyledText(
+                                    "Terms of use",
+                                    Modifier.clickable { navController.navigate(TermsOfUseScreen) })
+                                StyledText("Partners", Modifier.clickable { navController.navigate(PartnersScreen) })
+                                StyledText("Schedule", Modifier.clickable { navController.navigate(ScheduleScreen) })
+                                StyledText("Speakers", Modifier.clickable { navController.navigate(SpeakersScreen) })
+                            }
                         }
-                    }
-                    composable<AboutAppScreen> {
-                        AboutApp()
-                    }
-                    composable<AboutConferenceScreen> {
-                        AboutConference()
-                    }
-                    composable<CodeOfConductScreen> {
-                        CodeOfConduct()
-                    }
-                    composable<SettingsScreen> {
-                        Settings()
-                    }
-                    composable<TermsOfUseScreen> {
-                        TermsOfUse()
-                    }
-                    composable<PartnersScreen> {
-                        Partners()
-                    }
-                    composable<PartnerDetailsScreen> {
-                        PartnerDetails(it.toRoute<PartnerDetailsScreen>().partnerId)
-                    }
-                    composable<SpeakersScreen> {
-                        Speakers(service.speakers.collectAsState().value)
-                    }
-                    composable<SpeakerDetailsScreen>(typeMap = mapOf(typeOf<SpeakerId>() to SpeakerIdNavType)) {
-                        val speakerId = it.toRoute<SpeakerDetailsScreen>().speakerId
-                        Speaker(service.speakerById(speakerId))
-                    }
-                    composable<ScheduleScreen> {
-                        Schedule(service.agenda.collectAsState().value)
-                    }
-                    composable<TalkDetailsScreen>(typeMap = mapOf(typeOf<SessionId>() to SessionIdNavType)) {
-                        val sessionId = it.toRoute<TalkDetailsScreen>().talkId
-                        Session(service.sessionById(sessionId))
+                        composable<AboutAppScreen> {
+                            AboutApp()
+                        }
+                        composable<AboutConferenceScreen> {
+                            AboutConference()
+                        }
+                        composable<CodeOfConductScreen> {
+                            CodeOfConduct()
+                        }
+                        composable<SettingsScreen> {
+                            Settings()
+                        }
+                        composable<TermsOfUseScreen> {
+                            TermsOfUse()
+                        }
+                        composable<PartnersScreen> {
+                            Partners()
+                        }
+                        composable<PartnerDetailsScreen> {
+                            PartnerDetails(it.toRoute<PartnerDetailsScreen>().partnerId)
+                        }
+                        composable<SpeakersScreen> {
+                            Speakers(service.speakers.collectAsState().value)
+                        }
+                        composable<SpeakerDetailsScreen>(typeMap = mapOf(typeOf<SpeakerId>() to SpeakerIdNavType)) {
+                            val speakerId = it.toRoute<SpeakerDetailsScreen>().speakerId
+                            Speaker(service.speakerById(speakerId))
+                        }
+                        composable<ScheduleScreen> {
+                            Schedule(service.agenda.collectAsState().value)
+                        }
+                        composable<TalkDetailsScreen>(typeMap = mapOf(typeOf<SessionId>() to SessionIdNavType)) {
+                            val sessionId = it.toRoute<TalkDetailsScreen>().talkId
+                            Session(service.sessionById(sessionId))
+                        }
                     }
                 }
             }
