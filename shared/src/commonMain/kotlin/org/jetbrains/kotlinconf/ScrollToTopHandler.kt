615d9bb241f6ca8f70739719ad51c11e3629077c
Victor Kropp <victor.kropp@jetbrains.com>
2025-02-27 10:53:29 +0100

    re-implement ScrollToTopHandler for LazyList

--- shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/ScrollToTopHandler.kt
+++ shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/ScrollToTopHandler.kt
@@ -1,7 +1,11 @@
 package org.jetbrains.kotlinconf
 
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
 
 @Composable
 expect fun ScrollToTopHandler(scrollState: ScrollState)
+
+@Composable
+expect fun ScrollToTopHandler(listState: LazyListState)
--- shared/src/iosMain/kotlin/org/jetbrains/kotlinconf/ScrollToTopHandler.kt
+++ shared/src/iosMain/kotlin/org/jetbrains/kotlinconf/ScrollToTopHandler.kt
@@ -1,8 +1,11 @@
+@file:OptIn(ExperimentalForeignApi::class)
+
 package org.jetbrains.kotlinconf
 
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -20,7 +23,6 @@ import platform.UIKit.UIScrollView
 import platform.UIKit.UIScrollViewDelegateProtocol
 import platform.darwin.NSObject
 
-@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun ScrollToTopHandler(scrollState: ScrollState) {
     val coroutineScope = rememberCoroutineScope()
@@ -43,20 +45,8 @@ actual fun ScrollToTopHandler(scrollState: ScrollState) {
             }
         }
     }
-    val scrollView = remember {
-        UIScrollView(CGRectMake(0.0, 0.0, 10000.0, 1.0)).apply {
-            scrollsToTop = true
-            delegate = scrollViewDelegate
-            // without setting content size, scroll to top wouldn't work
-            setContentSize(CGSizeMake(10000.0, scrollState.viewportSize.toDouble()))
-        }
-    }
 
-    // Native view to handle system event
-    UIKitView(
-        factory = { scrollView },
-        modifier = Modifier.height(1.dp).fillMaxWidth()
-    )
+    val scrollView = UIScrollView(scrollViewDelegate)
 
     // Sync from Compose to Native
     LaunchedEffect(scrollState.value) {
@@ -65,4 +55,59 @@ actual fun ScrollToTopHandler(scrollState: ScrollState) {
             scrollView.setContentOffset(CGPointMake(0.0, scrollState.value.toDouble()))
         }
     }
+}
+
+@Composable
+actual fun ScrollToTopHandler(listState: LazyListState) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollViewDelegate = remember {
+        object : NSObject(), UIScrollViewDelegateProtocol {
+            // Prerequisites for the scroll-to-top gesture to work are (https://developer.apple.com/documentation/uikit/uiscrollview/scrollstotop):
+            // * scrollsToTop property set to true
+            // * scrollViewShouldScrollToTop should return true
+            // * current position shouldn't be at 0
+            override fun scrollViewShouldScrollToTop(scrollView: UIScrollView): Boolean = true
+
+            // Sync from Native to Compose
+            override fun scrollViewDidScroll(scrollView: UIScrollView) {
+                val currentOffset = scrollView.contentOffset.useContents { x.toInt() * FAKE_ITEM_SIZE + y.toInt() }
+                if (currentOffset != listState.firstVisibleItemIndex * FAKE_ITEM_SIZE + listState.firstVisibleItemScrollOffset) {
+                    coroutineScope.launch {
+                        listState.scrollToItem(currentOffset / FAKE_ITEM_SIZE, currentOffset % FAKE_ITEM_SIZE)
+                    }
+                }
+            }
+        }
+    }
+
+    val scrollView = UIScrollView(scrollViewDelegate)
+
+    // Sync from Compose to Native
+    LaunchedEffect(listState.firstVisibleItemIndex, listState.firstVisibleItemScrollOffset) {
+        val currentOffset = scrollView.contentOffset.useContents { x.toInt() * FAKE_ITEM_SIZE + y.toInt() }
+        if (currentOffset != listState.firstVisibleItemIndex * FAKE_ITEM_SIZE + listState.firstVisibleItemScrollOffset) {
+            scrollView.setContentOffset(CGPointMake(0.0, (listState.firstVisibleItemIndex * FAKE_ITEM_SIZE + listState.firstVisibleItemScrollOffset).toDouble()))
+        }
+    }
+}
+
+private const val FAKE_ITEM_SIZE = 1000
+
+@Composable
+private fun UIScrollView(scrollViewDelegate: UIScrollViewDelegateProtocol): UIScrollView {
+    val scrollView = remember {
+        UIScrollView(CGRectMake(0.0, 0.0, 10000.0, 1.0)).apply {
+            scrollsToTop = true
+            delegate = scrollViewDelegate
+            // without setting content size, scroll to top wouldn't work
+            setContentSize(CGSizeMake(10000.0, 10000.0))
+        }
+    }
+
+    // Native view to handle system event
+    UIKitView(
+        factory = { scrollView },
+        modifier = Modifier.height(1.dp).fillMaxWidth()
+    )
+    return scrollView
 }
