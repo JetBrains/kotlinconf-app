46da1468f3674178707642a1ad474f44c1957c33
Victor Kropp <victor.kropp@jetbrains.com>
2025-03-05 10:52:15 +0100

    update to the latest dev builds to fix compatibility issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
@@ -13,6 +14,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         mavenCentral()
