2d6924efdbd45e3954166d09750d7621e45bf1e8
Márton Braun <marton.braun@jetbrains.com>
2025-03-21 09:05:35 +0100

    Update normalize library to new version with wasm support

--- shared/src/webMain/kotlin/org/jetbrains/kotlinconf/utils/TextUtils.web.kt
+++
@@ -1,5 +0,0 @@
-package org.jetbrains.kotlinconf.utils
-
-actual fun String.normalizeNfd(): String = normalizeImpl(this)
-
-private fun normalizeImpl(str: String): String = js("""str.normalize("NFD")""")
