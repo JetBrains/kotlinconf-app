7674a14d2011f3a1d1f7a49b3882a28106e2cb41
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2025-03-24 20:14:25 +0100

    Web: set SerialName for route keys to get short and readable paths

--- shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/navigation/Routes.kt
+++ shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/navigation/Routes.kt
@@ -1,81 +1,107 @@
 package org.jetbrains.kotlinconf.navigation
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.jetbrains.kotlinconf.PartnerId
 import org.jetbrains.kotlinconf.SessionId
 import org.jetbrains.kotlinconf.SpeakerId
 
 @Serializable
+@SerialName("AboutConference")
 data object AboutConferenceScreen
 
 @Serializable
+@SerialName("CodeOfConduct")
 data object CodeOfConductScreen
 
 @Serializable
+@SerialName("AboutApp")
 data object AboutAppScreen
 
 @Serializable
+@SerialName("Info")
 data object InfoScreen
 
 @Serializable
+@SerialName("Welcome")
 data object StartScreens
 
 @Serializable
+@SerialName("WelcomePrivacyPolicy")
 data object StartPrivacyPolicyScreen
 
 @Serializable
+@SerialName("WelcomeSetupNotifications")
 data object StartNotificationsScreen
 
 @Serializable
+@SerialName("PrivacyPolicy")
 data object PrivacyPolicyScreen
 
 @Serializable
+@SerialName("Settings")
 data object SettingsScreen
 
 @Serializable
+@SerialName("PrivacyPolicyForVisitors")
 data object PrivacyPolicyForVisitorsScreen
 
 @Serializable
+@SerialName("AppPrivacyPolicy")
 data object AppPrivacyPolicyScreen
 
 @Serializable
+@SerialName("TermsOfUse")
 data object TermsOfUseScreen
 
 @Serializable
+@SerialName("AppTermsOfUse")
 data object AppTermsOfUseScreen
 
 @Serializable
+@SerialName("Licenses")
 data object LicensesScreen
 
 @Serializable
+@SerialName("License")
 data class SingleLicenseScreen(val licenseName: String, val licenseText: String)
 
 @Serializable
+@SerialName("Partners")
 data object PartnersScreen
 
 @Serializable
+@SerialName("Partner")
 data class PartnerDetailScreen(val partnerId: PartnerId)
 
 @Serializable
+@SerialName("Main")
 data class MainScreen(val roomNameForMap: String? = null)
 
 @Serializable
+@SerialName("Schedule")
 data object ScheduleScreen
 
 @Serializable
+@SerialName("Session")
 data class SessionScreen(val sessionId: SessionId)
 
 @Serializable
+@SerialName("Speakers")
 data object SpeakersScreen
 
 @Serializable
+@SerialName("Speaker")
 data class SpeakerDetailScreen(val speakerId: SpeakerId)
 
 @Serializable
+@SerialName("Map")
 data class MapScreenParam(val roomName: String? = null)
 
 @Serializable
+@SerialName("News")
 data object NewsListScreen
 
 @Serializable
+@SerialName("News")
 data class NewsDetailScreen(val newsId: String)
