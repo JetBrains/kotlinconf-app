33d5ca0a9a6c5b0ca25b24f5c38383e6a3e95e3f
Goooler <wangzongler@gmail.com>
2025-04-06 16:18:17 +0800

    Fix edge-to-edge on three-button nav bar

--- androidApp/src/androidMain/kotlin/org/jetbrains/kotlinconf/android/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/org/jetbrains/kotlinconf/android/MainActivity.kt
@@ -2,6 +2,7 @@ package org.jetbrains.kotlinconf.android
 
 import android.content.Intent
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
@@ -25,7 +26,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge()
 
         processIntent(intent)
 
@@ -41,6 +41,11 @@ class MainActivity : ComponentActivity() {
                             detectDarkMode = { isDarkMode }
                         )
                     )
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        // Fix for three-button nav not properly going edge-to-edge.
+                        // TODO: https://issuetracker.google.com/issues/298296168
+                        window.isNavigationBarContrastEnforced = false
+                    }
                 },
                 popEnterTransition = {
                     scaleIn(initialScale = 1.05f) +
