935c2153a01c7fd9895cb0dd6ff5f863c15fa7d8
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2025-04-14 09:34:08 +0200

    Update Compose Hot Reload to 1.0.0-alpha05

--- ui-components-gallery/src/jvmMain/kotlin/org/jetbrains/kotlinconf/ui/components/Main.kt
+++ ui-components-gallery/src/jvmMain/kotlin/org/jetbrains/kotlinconf/ui/components/Main.kt
@@ -4,7 +4,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import org.jetbrains.compose.reload.DevelopmentEntryPoint
 
 fun main() {
     application {
@@ -14,9 +13,7 @@ fun main() {
             state = rememberWindowState(width = 600.dp, height = 800.dp),
             title = "Gallery",
         ) {
-            DevelopmentEntryPoint {
-                GalleryApp()
-            }
+            GalleryApp()
         }
     }
 }
