0652d6b176cf02aa2621bee91dbf7ba29554f8c5
Márton Braun <marton.braun@jetbrains.com>
2025-04-20 07:36:39 +0200

    Fix typo in diacriticsRegex

--- shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/utils/TextUtils.kt
+++ shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/utils/TextUtils.kt
@@ -3,10 +3,10 @@ package org.jetbrains.kotlinconf.utils
 import doist.x.normalize.Form
 import doist.x.normalize.normalize
 
-private val diacricitsRegex = "\\p{Mn}+".toRegex()
+private val diacriticsRegex = "\\p{Mn}+".toRegex()
 
-fun String.containsDiacritics(): Boolean = normalizeNfd().contains(diacricitsRegex)
+fun String.containsDiacritics(): Boolean = normalizeNfd().contains(diacriticsRegex)
 
-fun String.removeDiacritics() = normalizeNfd().replace(diacricitsRegex, "")
+fun String.removeDiacritics() = normalizeNfd().replace(diacriticsRegex, "")
 
 private fun String.normalizeNfd(): String = normalize(Form.NFD)
