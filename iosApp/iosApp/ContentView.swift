1a246515f14f370f79da776bff78f4cd8b4501f4
Fedor Ihnatkevich <fedor.ihnatkevich@jetbrains.com>
2025-04-30 15:49:43 +0200

    Migrate XcodeProject to Amper

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -7,7 +7,7 @@
 //
 
 import SwiftUI
-import shared
+import KotlinModules
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,7 @@
 import FirebaseCore
 import FirebaseMessaging
 import SwiftUI
-import shared
+import KotlinModules
 
 @main
 struct iOSApp: App {
@@ -51,7 +51,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         _ application: UIApplication,
         didReceiveRemoteNotification userInfo: [AnyHashable: Any]
     ) async -> UIBackgroundFetchResult {
-        NotifierManager.shared.onApplicationDidReceiveRemoteNotification(userInfo: userInfo)
+        // NotifierManager.KotlinModules.onApplicationDidReceiveRemoteNotification(userInfo: userInfo)
         return UIBackgroundFetchResult.newData
     }
 
