7ff7956796fa1fb321ea2e9e5d522aea399d268b
Márton Braun <marton.braun@jetbrains.com>
2025-07-08 10:16:25 +0200

    Remove empty Client.kt

--- backend/src/main/kotlin/org/jetbrains/kotlinconf/backend/Client.kt
+++
@@ -1,6 +0,0 @@
-package org.jetbrains.kotlinconf.backend
-
-import io.ktor.client.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.Json
