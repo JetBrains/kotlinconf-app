c488068738b8807a07b43a1fe293c68a7d07a17a
Márton Braun <marton.braun@jetbrains.com>
2025-08-13 15:56:55 +0200

    Remove extra wrapper config

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,4 @@ plugins {
     alias(libs.plugins.googleServices) apply false
 }
 
-tasks.wrapper {
-    distributionType = Wrapper.DistributionType.BIN
-    gradleVersion = "8.10.2"
-    distributionUrl = "https://services.gradle.org/distributions/gradle-$gradleVersion-bin.zip"
-    distributionSha256Sum = "31c55713e40233a8303827ceb42ca48a47267a0ad4bab9177123121e71524c26"
-}
-
 apply(from = "gradle/releases.gradle.kts")
