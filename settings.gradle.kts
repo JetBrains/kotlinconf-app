ca8101f0ffbba991f91828ad04a2e7388fea36ad
Márton Braun <marton.braun@jetbrains.com>
2025-11-05 10:37:26 +0100

    Remove Foojay resolver plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,10 +34,6 @@ dependencyResolutionManagement {
     }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 include(":androidApp")
 include(":ui-components")
 include(":ui-components-gallery")
