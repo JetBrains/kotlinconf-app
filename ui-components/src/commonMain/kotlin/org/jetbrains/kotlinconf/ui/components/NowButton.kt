56ce344da44eff0e20063ebdb89d973eb9643b45
Victor Kropp <victor.kropp@jetbrains.com>
2025-11-07 10:10:31 +0100

    Remove empty line

--- ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/components/NowButton.kt
+++ ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/components/NowButton.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
