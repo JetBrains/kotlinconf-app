c26710a10273926a6fc55ab9676264b6b04c6062
Márton Braun <marton.braun@jetbrains.com>
2026-02-17 14:15:03 +0100

    Use styles API for styling Button and FilterTag

--- app/ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/components/FiltersTag.kt
+++ app/ui-components/src/commonMain/kotlin/org/jetbrains/kotlinconf/ui/components/FiltersTag.kt
@@ -1,12 +1,17 @@
+@file:OptIn(ExperimentalFoundationStyleApi::class)
+
 package org.jetbrains.kotlinconf.ui.components
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.style.ExperimentalFoundationStyleApi
+import androidx.compose.foundation.style.MutableStyleState
+import androidx.compose.foundation.style.Style
+import androidx.compose.foundation.style.StyleScope
+import androidx.compose.foundation.style.StyleStateKey
+import androidx.compose.foundation.style.styleable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,16 +19,42 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.jetbrains.kotlinconf.ui.theme.KotlinConfTheme
+import org.jetbrains.kotlinconf.ui.theme.LocalColors
 import org.jetbrains.kotlinconf.ui.theme.PreviewHelper
 
-
 private val FilterTagShape = RoundedCornerShape(size = 6.dp)
+
+private val TagSelectedStateKey = StyleStateKey<Boolean>(false)
+
+private var MutableStyleState.isTagSelected: Boolean
+    get() = this[TagSelectedStateKey]
+    set(value) { this[TagSelectedStateKey] = value }
+
+private fun StyleScope.tagSelected(style: Style) {
+    state(TagSelectedStateKey, style) { key, state -> state[key] == true }
+}
+
+private val filterTagStyle = Style {
+    shape(FilterTagShape)
+    border(1.dp, LocalColors.currentValue.strokeFull)
+    background(LocalColors.currentValue.mainBackground)
+    contentColor(LocalColors.currentValue.primaryText)
+    contentPaddingHorizontal(12.dp)
+    contentPaddingVertical(10.dp)
+
+    tagSelected {
+        animate {
+            borderColor(Color.Transparent)
+            background(LocalColors.currentValue.primaryBackground)
+            contentColor(LocalColors.currentValue.primaryTextWhiteFixed)
+        }
+    }
+}
 
 @Composable
 fun FilterTag(
@@ -32,37 +63,23 @@ fun FilterTag(
     onSelect: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val backgroundColor by animateColorAsState(
-        if (selected) KotlinConfTheme.colors.primaryBackground
-        else KotlinConfTheme.colors.mainBackground,
-        ColorSpringSpec,
-    )
-    val textColor by animateColorAsState(
-        if (selected) KotlinConfTheme.colors.primaryTextWhiteFixed
-        else KotlinConfTheme.colors.primaryText,
-        ColorSpringSpec,
-    )
-    val strokeColor by animateColorAsState(
-        if (selected) Color.Transparent
-        else KotlinConfTheme.colors.strokeFull,
-        ColorSpringSpec,
-    )
+    val interactionSource = remember { MutableInteractionSource() }
+    val styleState = remember(interactionSource) { MutableStyleState(interactionSource) }
+    styleState.isTagSelected = selected
+
+    val textColor = if (selected) KotlinConfTheme.colors.primaryTextWhiteFixed
+        else KotlinConfTheme.colors.primaryText
 
     Box(
         modifier = modifier
-            .clip(FilterTagShape)
-            .border(
-                width = 1.dp,
-                color = strokeColor,
-                shape = FilterTagShape,
-            )
             .selectable(
                 selected = selected,
+                interactionSource = interactionSource,
+                indication = null,
                 onClick = { onSelect(!selected) },
-                role = Role.RadioButton
+                role = Role.RadioButton,
             )
-            .background(backgroundColor)
-            .padding(horizontal = 12.dp, vertical = 10.dp),
+            .styleable(styleState, filterTagStyle),
         contentAlignment = Alignment.Center,
     ) {
         Text(
