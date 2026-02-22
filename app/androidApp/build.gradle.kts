008473e5855a739e6226d757674d7ed5c1277a07
Márton Braun <marton.braun@jetbrains.com>
2026-02-22 14:27:30 +0100

    Enable R8 minification for Android release builds

--- app/androidApp/build.gradle.kts
+++ app/androidApp/build.gradle.kts
@@ -45,7 +45,7 @@ android {
     buildTypes {
         release {
             isDebuggable = false
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
