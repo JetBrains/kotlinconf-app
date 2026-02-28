5a5cd662a919a329c997b39d33564530b8fa29fd
Márton Braun <marton.braun@jetbrains.com>
2026-02-28 09:15:29 +0100

    Set 2026 as current year

--- app/shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/storage/DefaultValues.kt
+++ app/shared/src/commonMain/kotlin/org/jetbrains/kotlinconf/storage/DefaultValues.kt
@@ -3,6 +3,6 @@ package org.jetbrains.kotlinconf.storage
 import org.jetbrains.kotlinconf.AppConfig
 
 val DEFAULT_CONFIG = AppConfig(
-    currentYear = 2025,
-    supportedYears = listOf(2025),
+    currentYear = 2026,
+    supportedYears = listOf(2026),
 )
