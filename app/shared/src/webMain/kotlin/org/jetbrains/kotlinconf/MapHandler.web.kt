bac622080384b5a44ce4e02d2a1e2fe8a3259852
Márton Braun <marton.braun@jetbrains.com>
2026-03-04 17:03:15 +0100

    Add kotlin-browser dependency for webMain use of browser APIs

--- app/shared/src/webMain/kotlin/org/jetbrains/kotlinconf/MapHandler.web.kt
+++ app/shared/src/webMain/kotlin/org/jetbrains/kotlinconf/MapHandler.web.kt
@@ -2,20 +2,19 @@ package org.jetbrains.kotlinconf
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import web.window.WindowTarget
+import web.window._blank
+import web.window.window
 import kotlin.js.js
 
 @Composable
 actual fun rememberMapHandler(): MapHandler = remember { WebMapHandler }
 
-internal external object window {
-    fun open(url: String, target: String)
-}
-
 private object WebMapHandler : MapHandler {
     override fun openNavigation(address: String) {
         val encodedAddress = encodeURIComponent(address)
         val url = "https://www.google.com/maps/search/?api=1&query=$encodedAddress"
-        window.open(url, "_blank")
+        window.open(url, WindowTarget._blank)
     }
 }
 
