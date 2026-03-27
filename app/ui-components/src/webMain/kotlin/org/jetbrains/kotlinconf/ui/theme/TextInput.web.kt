4bbcda5c55edf14daae1180ddf59acbe28a93974
Victor Kropp <victor.kropp@jetbrains.com>
2026-03-27 15:00:33 +0100

    enable Native Text Input on iOS

---
+++ app/ui-components/src/webMain/kotlin/org/jetbrains/kotlinconf/ui/theme/TextInput.web.kt
@@ -0,0 +1,7 @@
+package org.jetbrains.kotlinconf.ui.theme
+
+import androidx.compose.foundation.text.KeyboardOptions
+
+actual fun keyboardOptions(): KeyboardOptions {
+    return KeyboardOptions.Default
+}
