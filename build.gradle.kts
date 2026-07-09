8e051105924cfadd238664a9195a746b35e344b3
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2026-07-09 17:50:46 +0100

    Prepare the project according to KT-75078

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsEnvSpec
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin
 import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsEnvSpec
@@ -29,6 +30,52 @@ allprojects {
     plugins.withType<WasmNodeJsPlugin> {
         extensions.configure<WasmNodeJsEnvSpec> { version.set(pinnedNodeVersion) }
     }
+
+    val kotlinLanguageVersionOverride = providers.gradleProperty("kotlin_language_version")
+        .map(org.jetbrains.kotlin.gradle.dsl.KotlinVersion::fromVersion)
+        .orNull
+    val kotlinApiVersionOverride = providers.gradleProperty("kotlin_api_version")
+        .map(org.jetbrains.kotlin.gradle.dsl.KotlinVersion::fromVersion)
+        .orNull
+    val kotlinAdditionalCliOptions = providers.gradleProperty("kotlin_additional_cli_options")
+        .map { it.split(" ").filter { option -> option.isNotEmpty() } }
+        .orNull
+
+    plugins.withId("org.jetbrains.kotlin.multiplatform") {
+        extensions.configure(KotlinMultiplatformExtension::class.java) {
+            compilerOptions {
+                kotlinLanguageVersionOverride?.let {
+                    languageVersion.set(it)
+                    logger.info("<KUP> ${project.path} : set LV to $it")
+                }
+
+                kotlinApiVersionOverride?.let {
+                    apiVersion.set(it)
+                    logger.info("<KUP> ${project.path} : set APIV to $it")
+                }
+
+                kotlinAdditionalCliOptions?.let {
+                    freeCompilerArgs.addAll(it)
+                    logger.info(
+                        "<KUP> ${project.path} : added ${
+                            kotlinAdditionalCliOptions.joinToString(
+                                " "
+                            )
+                        }"
+                    )
+                }
+
+                freeCompilerArgs.add("-Xreport-all-warnings")
+                logger.info("<KUP> ${project.path} : added -Xreport-all-warnings")
+                // output kotlin.git-searchable names of reported diagnostics
+                freeCompilerArgs.add("-Xrender-internal-diagnostic-names")
+                logger.info("<KUP> ${project.path} : added -Xrender-internal-diagnostic-names")
+
+                allWarningsAsErrors.set(false)
+            }
+        }
+    }
+
 }
 
 apply(from = "gradle/releases.gradle.kts")
